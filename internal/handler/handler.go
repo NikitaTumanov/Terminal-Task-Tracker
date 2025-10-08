02756d55bb47a86c9648268c86b81cb778fc73e1
Nikita Tumanov <toomuchick@gmail.com>
2025-10-08 20:59:10 +0300

    add all handlers

---
+++ internal/handler/handler.go
@@ -0,0 +1,151 @@
+package handler
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/NikitaTumanov/terminalTaskTracker/internal/taskstorage"
+)
+
+func Read() string {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimRight(input, "\r\n")
+
+		if input != "" {
+			return input
+		}
+
+		fmt.Println("Чтобы получить информацию о доступных командах, введите help")
+	}
+}
+
+func splitInput(input string) []string {
+    var result []string
+    var current strings.Builder
+    inQuotes := false
+    quoteChar := rune(0)
+
+    for _, char := range input {
+        switch {
+        case char == '"' || char == '\'':
+            if !inQuotes {
+                inQuotes = true
+                quoteChar = char
+            } else if char == quoteChar {
+                inQuotes = false
+                quoteChar = rune(0)
+            }
+            current.WriteRune(char)
+        case char == ' ' && !inQuotes:
+            if current.Len() > 0 {
+                result = append(result, current.String())
+                current.Reset()
+            }
+        default:
+            current.WriteRune(char)
+        }
+    }
+
+    if current.Len() > 0 {
+        result = append(result, current.String())
+    }
+
+    return result
+}
+
+func Handle() {
+	var task taskstorage.Task
+
+	for {
+		fmt.Print("Enter command: ")
+		input := Read()
+		elements := splitInput(input)
+
+		switch strings.ToLower(elements[0]) {
+		case "add":
+			result, err := task.Add(elements[1:])
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			fmt.Println(result)
+		case "update":
+			result, err := task.Update(elements[1:])
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			fmt.Println(result)
+		case "delete":
+			result, err := task.Delete(elements[1:])
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			fmt.Println(result)
+		case "updatestatus":
+			result, err := task.UpdateStatus(elements[1:])
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			fmt.Println(result)
+		case "alltasks":
+			result, err := taskstorage.AllTasks()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			fmt.Println(result)
+		case "donetasks":
+			result, err := taskstorage.DoneTasks()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			fmt.Println(result)
+		case "notdonetasks":
+			result, err := taskstorage.NotDoneTasks()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			fmt.Println(result)
+		case "inprogresstasks":
+			result, err := taskstorage.InProgressTasks()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			fmt.Println(result)
+		case "help":
+			fmt.Println(`	Add "<Task name>"
+	Update <Task Index> <New Task Name> <New Task Status>
+		Task Statuses:
+			0 - Не начато
+			1 - В процессе
+			2 - Выполнено
+	Delete <Task Index>
+	UpdateStatus <Task Index> <New Task Status>
+		Task Statuses:
+			0 - Не начато
+			1 - В процессе
+			2 - Выполнено
+	AllTasks
+	DoneTasks
+	NotDoneTasks
+	InProgressTasks
+	Help
+	Exit`)
+		case "exit":
+			return
+		default:
+			fmt.Println("Введена некорректная команда")
+		}
+	}
+}
--- internal/taskstorage/taskstorage.go
+++ internal/taskstorage/taskstorage.go
@@ -4,31 +4,28 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-
-	// "fmt"
-	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type TaskStatus int
-
-const (
-	TasksPath   = "tasks.json"
-	CounterPath = "count.txt"
-)
-
-const (
-	StatusNotDone TaskStatus = iota
-	StatusInProgress
-	StatusDone
-)
 
 type Task struct {
 	Index  int        `json:"index"`
 	Name   string     `json:"name"`
 	Status TaskStatus `json:"status"`
 }
+
+const (
+	StatusNotDone TaskStatus = iota
+	StatusInProgress
+	StatusDone
+	TasksPath = "tasks.json"
+)
+
+var errInputElementsCount error = errors.New("передано некорректное количество аргументов")
+var errAtoi error = errors.New("введено некорректное число")
 
 func printTasks(tasks []Task) string {
 	var result string
@@ -42,123 +39,193 @@ func printTasks(tasks []Task) string {
 		case StatusNotDone:
 			status = "Не начато"
 		}
-
-		result += fmt.Sprintf(`Index: %d
-Name: %s
-Status: %s
-
-`, task.Index, task.Name, status)
+		result += fmt.Sprintf("Index: %d\nName: %s\nStatus: %s\n", task.Index, task.Name, status)
 	}
 	return result
 }
 
+func addToFile(allTasks []Task) error {
+	tasksJSON, err := json.MarshalIndent(allTasks, "", "\t")
+	if err != nil {
+		return errors.New("ошибка при сериализации в JSON")
+	}
+
+	err = os.WriteFile(TasksPath, []byte(tasksJSON), 0644)
+	if err != nil {
+		return errors.New("ошибка при записи в файл")
+	}
+
+	return nil
+}
+
+func getAllTasks() ([]Task, error) {
+	data, err := os.ReadFile(TasksPath)
+	if err != nil {
+		return nil, errors.New("ошибка при чтении файла")
+	}
+
+	var allTasks []Task
+	if len(data) == 0 {
+		return allTasks, nil
+	}
+
+	err = json.Unmarshal(data, &allTasks)
+	if err != nil {
+		return nil, errors.New("ошибка при десериализации из JSON")
+	}
+
+	return allTasks, nil
+}
+
 func (t Task) Add(elements []string) (string, error) {
 	if len(elements) != 1 {
-		return "", errors.New("передано некорректное количество аргументов")
+		return "", errInputElementsCount
 	}
 
-	countStr, err := os.ReadFile(CounterPath)
-	if err != nil {
-		return "", err
-	}
-	count, err := strconv.Atoi(string(countStr))
-	if err != nil {
-		return "", err
-	}
-	t.Index = count + 1
-	t.Name = elements[0]
-
-	file, err := os.OpenFile(TasksPath, os.O_RDWR|os.O_CREATE, 0644)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	stat, err := file.Stat()
+	allTasks, err := getAllTasks()
 	if err != nil {
 		return "", err
 	}
 
-	if stat.Size() == 0 {
-		// Файл пустой - пишем начальный массив
-		_, err = file.WriteString("[\n\t")
+	t.Index = len(allTasks) + 1
+	elements[0] = strings.ReplaceAll(elements[0],"\"", "")
+	t.Name = strings.ReplaceAll(elements[0],"'", "")
+	if t.Name == ""{
+		return "", errors.New("введено пустое имя задачи")
+	}
+
+	allTasks = append(allTasks, t)
+
+	err = addToFile(allTasks)
+	if err != nil {
+		return "", err
+	}
+
+	return "Задача добавлена", nil
+}
+
+func (t Task) Update(elements []string) (string, error) {
+	if len(elements) != 3 {
+		return "", errInputElementsCount
+	}
+
+	allTasks, err := getAllTasks()
+	if err != nil {
+		return "", err
+	}
+
+	for i := 0; i < len(allTasks); i++ {
+		index, err := strconv.Atoi(elements[0])
 		if err != nil {
-			return "", err
+			return "", errAtoi
 		}
-	} else {
-		// Перемещаемся перед закрывающей скобкой
-		file.Seek(-1, 2) // Последний символ
-		lastChar := make([]byte, 1)
-		file.Read(lastChar)
 
-		if lastChar[0] == ']' {
-			// Массив не пустой - добавляем запятую и новую строку
-			file.Seek(-1, 2)
-			_, err = file.WriteString(",\n\t")
+		if allTasks[i].Index == index {
+			status, err := strconv.Atoi(elements[2])
+			if err != nil {
+				return "", errAtoi
+			}
+
+			t.Index = index
+			t.Name, allTasks[i].Name = elements[1], elements[1]
+			t.Status, allTasks[i].Status = TaskStatus(status), TaskStatus(status)
+
+			err = addToFile(allTasks)
 			if err != nil {
 				return "", err
 			}
+
+			return printTasks([]Task{t}), nil
 		}
 	}
 
-	taskJSON, err := json.MarshalIndent(t, "\t", "\t")
+	return "Задача не найдена", nil
+}
+
+func (t Task) Delete(elements []string) (string, error) {
+	if len(elements) != 1 {
+		return "", errInputElementsCount
+	}
+
+	allTasks, err := getAllTasks()
 	if err != nil {
 		return "", err
 	}
 
-	_, err = file.Write(taskJSON)
+	for i := 0; i < len(allTasks); i++ {
+		index, err := strconv.Atoi(elements[0])
+		if err != nil {
+			return "", errAtoi
+		}
+
+		if allTasks[i].Index == index {
+			allTasks = append(allTasks[:i], allTasks[i+1:]...)
+			err = addToFile(allTasks)
+			if err != nil {
+				return "", err
+			}
+			
+			return printTasks(allTasks), nil
+		}
+	}
+
+	return "Задача не найдена", nil
+}
+
+func (t Task) UpdateStatus(elements []string) (string, error) {
+	if len(elements) != 2 {
+		return "", errInputElementsCount
+	}
+
+	allTasks, err := getAllTasks()
 	if err != nil {
 		return "", err
 	}
 
-	_, err = file.WriteString("\n]")
-	if err != nil {
-		log.Fatal(err)
+	for i := 0; i < len(allTasks); i++ {
+		index, err := strconv.Atoi(elements[0])
+		if err != nil {
+			return "", errAtoi
+		}
+
+		if allTasks[i].Index == index {
+			status, err := strconv.Atoi(elements[1])
+			if err != nil {
+				return "", errAtoi
+			}
+
+			t.Index = index
+			t.Name = allTasks[i].Name
+			t.Status, allTasks[i].Status = TaskStatus(status), TaskStatus(status)
+
+			err = addToFile(allTasks)
+			if err != nil {
+				return "", err
+			}
+
+			return printTasks([]Task{t}), nil
+		}
 	}
+	return "Задача не найдена", nil
+}
 
-	err = os.WriteFile(CounterPath, []byte(strconv.Itoa(t.Index)), 0644)
+func AllTasks() (string, error) {
+	allTasks, err := getAllTasks()
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
-
-	return "Задача добавлена", err
+	return printTasks(allTasks), nil
 }
 
-func (t Task) Update(elements []string) {
-	//TODO
-}
-
-func (t Task) Delete(elements []string) {
-	//TODO
-}
-
-func (t Task) UpdateStatus(elements []string) {
-	//TODO
-}
-
-func (t Task) AllTasks() (string, error) {
-	var tasks []Task
-
-	data, err := os.ReadFile(TasksPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = json.Unmarshal(data, &tasks)
-	return printTasks(tasks), nil
-}
-
-func (t Task) DoneTasks() (string, error) {
-	var tasks []Task
+func DoneTasks() (string, error) {
 	var result []Task
 
-	data, err := os.ReadFile(TasksPath)
+	allTasks, err := getAllTasks()
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
-	err = json.Unmarshal(data, &tasks)
-	for _, task := range tasks {
+	for _, task := range allTasks {
 		if task.Status == StatusDone {
 			result = append(result, task)
 		}
@@ -166,17 +233,15 @@ func (t Task) DoneTasks() (string, error) {
 	return printTasks(result), nil
 }
 
-func (t Task) NotDoneTasks() (string, error) {
-	var tasks []Task
+func NotDoneTasks() (string, error) {
 	var result []Task
 
-	data, err := os.ReadFile(TasksPath)
+	allTasks, err := getAllTasks()
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
-	err = json.Unmarshal(data, &tasks)
-	for _, task := range tasks {
+	for _, task := range allTasks {
 		if task.Status == StatusNotDone {
 			result = append(result, task)
 		}
@@ -184,17 +249,15 @@ func (t Task) NotDoneTasks() (string, error) {
 	return printTasks(result), nil
 }
 
-func (t Task) InProgressTasks() (string, error) {
-	var tasks []Task
+func InProgressTasks() (string, error) {
 	var result []Task
 
-	data, err := os.ReadFile(TasksPath)
+	allTasks, err := getAllTasks()
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
-	err = json.Unmarshal(data, &tasks)
-	for _, task := range tasks {
+	for _, task := range allTasks {
 		if task.Status == StatusInProgress {
 			result = append(result, task)
 		}
